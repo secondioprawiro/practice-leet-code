83455891f955a3c4a3b7bd718ca157a8a5833a5a
secondio10 <168173824+secondioprawiro@users.noreply.github.com>
2026-01-27 23:33:23 +0700

    add no 2

---
+++ medium/2. Add Two Numbers/add_two_numbers.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int temp = 0;
+
+        while(l1 != null || l2 != null || temp != 0){
+            int sum = temp;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            temp = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;        
+        }
+
+        return dummy.next;
+    }
+}
